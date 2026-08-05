b89ac0ea77a83f946ed04af08dd96c24c6fc5940
Manshavatsh0123 <manshavatsh@gmail.com>
2026-08-05 22:13:53 +0530

    Set up Express server and project architecture

---
+++ src/config/db.js
@@ -0,0 +1,19 @@
+const { Pool } = require("pg");
+
+const env = require("./env");
+
+const pool = new Pool({
+
+    host: env.DB_HOST,
+
+    port: env.DB_PORT,
+
+    user: env.DB_USER,
+
+    password: env.DB_PASSWORD,
+
+    database: env.DB_NAME
+
+});
+
+module.exports = pool;
---
+++ src/server.js
@@ -0,0 +1,9 @@
+const app = require("./app");
+
+const env = require("./config/env");
+
+app.listen(env.PORT, () => {
+
+    console.log(`Server running on port ${env.PORT}`);
+
+});
